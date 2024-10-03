1e07f774b3e97a13b3dbbacfee9bff0ed1dfa033
marianafaustino <marifaustino@gmail.com>
2024-10-03 17:28:12 -0300

    feat: inicializa configurações do typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuariosModule],
+  imports: [
+    UsuariosModule,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [`${__dirname}/typeorm/entities/*{.js,.ts}`],
+      synchronize: true, 
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
